4d7edd44296f77622e4baf20df941bc3bbda6266
Avnish <Avnishway@gmail.com>
2019-01-29 07:36:48 +0530

    Updates

---
+++ precache-manifest.e4309ebf51e320135e909acd9ffb4483.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "18dfca7d53a6a114a445ea2104e94da1",
+    "url": "/studentsdata.github.io/static/media/Screen Shot 2019-01-23 at 6.53.41 PM.18dfca7d.png"
+  },
+  {
+    "revision": "aab297eb900a2eb062e5",
+    "url": "/studentsdata.github.io/static/js/runtime~main.aab297eb.js"
+  },
+  {
+    "revision": "2a1d06ea671b3ceb4613",
+    "url": "/studentsdata.github.io/static/js/main.2a1d06ea.chunk.js"
+  },
+  {
+    "revision": "e5593c840306af6de354",
+    "url": "/studentsdata.github.io/static/js/1.e5593c84.chunk.js"
+  },
+  {
+    "revision": "2a1d06ea671b3ceb4613",
+    "url": "/studentsdata.github.io/static/css/main.e366d52b.chunk.css"
+  },
+  {
+    "revision": "e5593c840306af6de354",
+    "url": "/studentsdata.github.io/static/css/1.28e1942a.chunk.css"
+  },
+  {
+    "revision": "4d3e4bc0a85fd668bd56d7ad5c4d0d67",
+    "url": "/studentsdata.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bc9d69886ad7c5004e24220a7b12870c.js"
+  "/studentsdata.github.io/precache-manifest.e4309ebf51e320135e909acd9ffb4483.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/studentsdata.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
