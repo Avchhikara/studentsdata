4c7d4c60552f89b3db89b23ca72d4485ef368c89
Avnish <Avnishway@gmail.com>
2019-01-29 07:25:56 +0530

    Updates

---
+++ precache-manifest.a33b0f9e26afa31ca2b541e2218f33a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "18dfca7d53a6a114a445ea2104e94da1",
+    "url": "/static/media/Screen Shot 2019-01-23 at 6.53.41 PM.18dfca7d.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7584ce4988b09c1a7aae",
+    "url": "/static/js/main.7584ce49.chunk.js"
+  },
+  {
+    "revision": "e5593c840306af6de354",
+    "url": "/static/js/1.e5593c84.chunk.js"
+  },
+  {
+    "revision": "7584ce4988b09c1a7aae",
+    "url": "/static/css/main.e366d52b.chunk.css"
+  },
+  {
+    "revision": "e5593c840306af6de354",
+    "url": "/static/css/1.28e1942a.chunk.css"
+  },
+  {
+    "revision": "8da5e2dce78fe95299891f4c197bed93",
+    "url": "/index.html"
+  }
+];
