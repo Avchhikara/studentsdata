240dd55742e9975d0c276d62a2c6e6ce427416b0
Avnish <Avnishway@gmail.com>
2019-02-28 07:52:59 +0530

    Adding teacher functionality

--- src/components/Teacher/TeacherSignup/TeacherSignup.js
+++ src/components/Teacher/TeacherSignup/TeacherSignup.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./TeacherSignup.css";
 
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { fetchURL } from "./../../../Actions/constants";
 
 import {
   Card,
@@ -21,7 +23,12 @@ class TeacherSignup extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      res: { status: "", message: "This feature is not currently working" }
+      res: { status: "", message: "This feature is not currently working" },
+      name: "",
+      email: "",
+      password: "",
+      university: "",
+      department: ""
     };
   }
 
@@ -33,6 +40,64 @@ class TeacherSignup extends React.Component {
   scrollToTop = () => {
     const cardTitle = document.querySelector(".card-title");
     cardTitle.scrollIntoView({ behavior: "smooth" });
+  };
+
+  onSignup = e => {
+    //Disabling the signup btn
+    const target = e.target;
+    target.disabled = true;
+    target.textContent = "Please wait..";
+
+    //First, checking that all values have been provided
+    const { name, email, password, university, department } = this.state;
+    if (
+      name !== "" &&
+      email !== "" &&
+      password !== "" &&
+      university !== "" &&
+      department !== ""
+    ) {
+      //What to do when all the columns are filled
+
+      const send = {
+        name,
+        email,
+        password,
+        university,
+        department
+      };
+
+      //Now, sending request to backend
+      axios
+        .post(`${fetchURL}/teacher/signup`, send)
+        .then(({ data }) => {
+          //updating the state
+          this.setState({ res: data.res });
+
+          //Enabling the btn
+          target.disabled = false;
+          target.textContent = "SignUp";
+
+          //Scrolling to top
+          this.scrollToTop();
+        })
+        .catch(err => {
+          console.log(err);
+          //Enabling the btn
+          target.disabled = false;
+          target.textContent = "SignUp";
+        });
+    } else {
+      //When any of the filed is vacant
+      this.setState({
+        res: { status: 404, message: "Please fill all the columns" }
+      });
+      //Enabling the btn
+      target.disabled = false;
+      target.textContent = "SignUp";
+      //Moving to top
+      this.scrollToTop();
+    }
   };
 
   render() {
@@ -62,7 +127,16 @@ class TeacherSignup extends React.Component {
                       Name
                     </Label>
                     <Col xs={9}>
-                      <Input type="text" id="name" name="name" />
+                      <Input
+                        type="text"
+                        id="name"
+                        name="name"
+                        value={this.state.name}
+                        onChange={e => {
+                          const val = e.target.value;
+                          this.setState({ name: val });
+                        }}
+                      />
                     </Col>
                   </FormGroup>
                   <FormGroup row>
@@ -70,7 +144,16 @@ class TeacherSignup extends React.Component {
                       Email
                     </Label>
                     <Col xs={9}>
-                      <Input type="email" id="email" name="email" />
+                      <Input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={this.state.email}
+                        onChange={e => {
+                          const val = e.target.value.toLowerCase();
+                          this.setState({ email: val });
+                        }}
+                      />
                     </Col>
                   </FormGroup>
                   <FormGroup row>
@@ -78,7 +161,16 @@ class TeacherSignup extends React.Component {
                       Password
                     </Label>
                     <Col xs={8} sm={9}>
-                      <Input type="password" id="password" name="password" />
+                      <Input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={e => {
+                          const val = e.target.value;
+                          this.setState({ password: val });
+                        }}
+                      />
                     </Col>
                   </FormGroup>
                   <FormGroup row>
@@ -86,7 +178,20 @@ class TeacherSignup extends React.Component {
                       University
                     </Label>
                     <Col xs={8} sm={9}>
-                      <Input type="select" id="university" name="university">
+                      <Input
+                        type="select"
+                        id="university"
+                        name="university"
+                        value={this.state.university}
+                        onChange={e => {
+                          const val = e.target.value;
+                          if (val !== "--Select University--") {
+                            this.setState({ university: val });
+                          } else {
+                            this.setState({ university: "" });
+                          }
+                        }}
+                      >
                         <option defaultValue>--Select University--</option>
                         <option value="DCRUSTM">D.C.R.U.S.T. Murthal</option>
                       </Input>
@@ -97,7 +202,20 @@ class TeacherSignup extends React.Component {
                       Dept.
                     </Label>
                     <Col xs={9}>
-                      <Input type="select" name="department" id="department">
+                      <Input
+                        type="select"
+                        name="department"
+                        id="department"
+                        value={this.state.department}
+                        onChange={e => {
+                          const val = e.target.value;
+                          if (val !== "--Select Department--") {
+                            this.setState({ department: val });
+                          } else {
+                            this.setState({ department: "" });
+                          }
+                        }}
+                      >
                         <option defaultValue>--Select Department--</option>
                         <option value="ECE">ECE</option>
                         <option value="CE">CE(Civil)</option>
@@ -114,7 +232,7 @@ class TeacherSignup extends React.Component {
               </CardBody>
               <Row>
                 <Col style={{ margin: "0 auto" }} xs={5}>
-                  <Button color="success" block>
+                  <Button color="success" block onClick={this.onSignup}>
                     SignUp
                   </Button>
                 </Col>
